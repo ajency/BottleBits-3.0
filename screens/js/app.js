9269a9515769449e4ebb42b80132f8632b717903
Ankita <ankita@ajency.in>
2021-08-27 14:35:36 +0530

    updated ui

--- screens/js/app.js
+++ screens/js/app.js
@@ -1,6 +1,33 @@
 $(document).ready(function(){
-	$('.shelf-block').on('click', function(){
-		$(this).addClass('expand');
-		$(this).after('<div class="shelf-block"></div>');
-	});
+	$(".shelf .shelf-block").addClass(function(i) { return "block" + (i + 1) })
+
+	$('.shelf .shelf-block').on('click', function(){
+		$(".slider .shelf-block").addClass('shelf-block--slider');
+		if($(this).hasClass('block1')){
+			$('.slider .shelf-block--details').removeClass('expand');
+			$('.slider').find('.shelf-block--details:nth-child(1)').addClass('expand');
+		}
+		if($(this).hasClass('block2')){
+			$('.slider .shelf-block--details').removeClass('expand');
+			$('.slider').find('.shelf-block--details:nth-child(2)').addClass('expand');
+		}
+		if($(this).hasClass('block3')){
+			$('.slider .shelf-block--details').removeClass('expand');
+			$('.slider').find('.shelf-block--details:nth-child(3)').addClass('expand');
+		}
+		if($(this).hasClass('block4')){
+			$('.slider .shelf-block--details').removeClass('expand');
+			$('.slider').find('.shelf-block--details:nth-child(4)').addClass('expand');
+		}
+		$('.podium').addClass('podium-animate');
+   });
+
+
+	// $('.product-slider').slick({
+	// 	dots: false,
+	// 	arrows: false,
+	// 	speed: 300,
+	// 	slidesToShow: 1,
+	// 	centerMode: true,
+	// });
 });
