e20c1d95b551c8c623836c9a25e958d69e3d14c5
Ankita <ankita@ajency.in>
2021-08-30 11:48:35 +0530

    build

--- screens/js/app.js
+++ screens/js/app.js
@@ -1,48 +1,61 @@
-$(document).ready(function(){
+
+$(document).ready(function(){	
 	$(".shelf .shelf-block").addClass(function(i) { return "block" + (i + 1) })
 
 	$('.shelf .shelf-block').on('click', function(){
-		$(".slider .shelf-block").addClass('shelf-block--slider');
+		$(".carousel .shelf-block").addClass('show_bg');
+		$(".carousel .shelf-block .shelf-block--wrap").addClass('shelf-block--slider');
+
 		if($(this).hasClass('block1')){
-			$('.slider .shelf-block--details').removeClass('expand');
-			$('.slider').find('.shelf-block--details:nth-child(1)').addClass('expand');
+			$('.carousel').find('.shelf-block--details .shelf-block--wrap.wrap1').prev().addClass('prev');
+			$('.carousel').find('.shelf-block--details .shelf-block--wrap.wrap1').addClass('active');
+			$('.carousel').find('.shelf-block--details .shelf-block--wrap.wrap1').next().addClass('next');
 		}
 		if($(this).hasClass('block2')){
-			$('.slider .shelf-block--details').removeClass('expand');
-			$('.slider').find('.shelf-block--details:nth-child(2)').addClass('expand');
+			$('.carousel').find('.shelf-block--details .shelf-block--wrap.wrap2').prev().addClass('prev');
+			$('.carousel').find('.shelf-block--details .shelf-block--wrap.wrap2').addClass('active');
+			$('.carousel').find('.shelf-block--details .shelf-block--wrap.wrap2').next().addClass('next');
 		}
 		if($(this).hasClass('block3')){
-			$('.slider .shelf-block--details').removeClass('expand');
-			$('.slider').find('.shelf-block--details:nth-child(3)').addClass('expand');
+			$('.carousel').find('.shelf-block--details .shelf-block--wrap.wrap3').prev().addClass('prev');
+			$('.carousel').find('.shelf-block--details .shelf-block--wrap.wrap3').addClass('active');
+			$('.carousel').find('.shelf-block--details .shelf-block--wrap.wrap3').next().addClass('next');
 		}
 		if($(this).hasClass('block4')){
-			$('.slider .shelf-block--details').removeClass('expand');
-			$('.slider').find('.shelf-block--details:nth-child(4)').addClass('expand');
+			$('.carousel').find('.shelf-block--details .shelf-block--wrap.wrap4').prev().addClass('prev');
+			$('.carousel').find('.shelf-block--details .shelf-block--wrap.wrap4').addClass('active');
+			$('.carousel').find('.shelf-block--details .shelf-block--wrap.wrap1').addClass('next');
 		}
 		$('.podium').addClass('podium-animate');
-   });
+   	});
 
-	$('.clickable-area .left').on('click', function(){
-		$(".shelf-block--slider.expand").prev().removeClass('right');
-		$(".shelf-block--slider.expand").next().removeClass('right');
 
-		$(".shelf-block--slider.expand").removeClass('right');
-		$(".shelf-block--slider.expand").prev().addClass('left');
+});
+$(".carousel").swipe({
+    swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
+        if (direction == 'left') $(this).carousel('next');
+        if (direction == 'right') $(this).carousel('prev');
+    },
+    allowPageScroll: "vertical"
+});
 
-		$(".shelf-block--slider.expand").addClass('left');
-		$(".shelf-block--slider.expand").prev().removeClass('wrap-right');
-		$(".shelf-block--slider.expand").removeClass('wrap-left');
-	});
-	$('.clickable-area .right').on('click', function(){
-		$(".shelf-block--slider.expand").removeClass('left');
-		$(".shelf-block--slider.expand").prev().removeClass('right');
 
-		$(".shelf-block--slider.expand").next().removeClass('left');
-		$(".shelf-block--slider.expand").next().addClass('right');
+var $carousel = $('.carousel'),
+    $carouselItems = $('.carousel-item', $carousel);
 
-		$(".shelf-block--slider.expand").addClass('right');
-		$(".shelf-block--slider.right").next().removeClass('wrap-left');
-
-		$(".shelf-block--slider.expand").removeClass('wrap-right');
-	});
+//This event is fired when the carousel has completed its slide transition.
+	$carousel.on('slid.bs.carousel', function (e) {
+    //Reset classes
+    $carouselItems.removeClass('prev next');
+    //Find current slide
+    var $active = $(e.relatedTarget);
+    if(($active).hasClass('wrap4')){
+    	$('.carousel').find('.shelf-block--details .shelf-block--wrap.wrap1').addClass('next');
+    }else{
+    	$active.next().addClass('next');
+	}
+	if(($active).hasClass('wrap1')){
+		$('.carousel').find('.shelf-block--details .shelf-block--wrap.wrap4').addClass('prev');
+	}
+	$active.prev().addClass('prev');
 });
